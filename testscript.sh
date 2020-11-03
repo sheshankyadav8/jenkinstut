823a395df6b3fbd198b780fa1f8060ace7a09b2a
sheshankyadav8 <sheshankyadav8@gmail.com>
2020-11-03 07:01:58 +0000

    removing script

--- testscript.sh
+++ testscript.sh
@@ -4,4 +4,7 @@ echo "this is a sample "
 echo "that is for github"
 NUMBER=$(($NUMBERONE + $NUMBERTWO))
 echo "$NUMBER"
-
+echo "this is done"
+echo "this is finished"
+echo "this poradadam"
+echo "i will get this"
